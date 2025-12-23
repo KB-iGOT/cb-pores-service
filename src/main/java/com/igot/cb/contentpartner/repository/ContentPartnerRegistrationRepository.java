b8220cc90a46d4d6969410633d45f088da4e4d4c
SnehaRAvindran <sneha.ravindran@tarento.com>
2025-12-23 17:46:49 +0530

    formatted code

--- src/main/java/com/igot/cb/contentpartner/repository/ContentPartnerRegistrationRepository.java
+++ src/main/java/com/igot/cb/contentpartner/repository/ContentPartnerRegistrationRepository.java
@@ -17,8 +17,7 @@ public interface ContentPartnerRegistrationRepository extends JpaRepository<Cont
             value = """
         SELECT * FROM content_partner_registration
         WHERE data ->> 'email' = :email
-        """,
-            nativeQuery = true
+        """, nativeQuery = true
     )
     Optional<ContentPartnerRegistrationEntity> findByEmail(@Param("email") String email);
     @Query(
@@ -26,12 +25,8 @@ public interface ContentPartnerRegistrationRepository extends JpaRepository<Cont
         SELECT * FROM content_partner_registration
         WHERE id = :id
         AND data ->> 'email' = :email
-        """,
-            nativeQuery = true
+        """, nativeQuery = true
     )
-    Optional<ContentPartnerRegistrationEntity> findByIdAndEmail(
-            @Param("id") String id,
-            @Param("email") String email
-    );
+    Optional<ContentPartnerRegistrationEntity> findByIdAndEmail(@Param("id") String id, @Param("email") String email);
 
 }
